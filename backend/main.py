6b4f55a847f7ade15f636fbd8570a6cb358d6103
UIE47061 <ue4761@gmail.com>
2025-08-29 16:24:16 +0800

    none

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from backend.api import ai_api
 from fastapi.middleware.cors import CORSMiddleware
 from backend.api import participants_api
-from backend.api import utility
 from backend.api import network_api
 
 app = FastAPI(title="MBBuddy API")
